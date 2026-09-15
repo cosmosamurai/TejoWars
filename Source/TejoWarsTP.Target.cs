64daa94c5235a220925e98a0207a2778512cd097
Renzo Ortube <117328374+cosmosamurai@users.noreply.github.com>
2026-09-15 19:58:56 -0300

    V1

--- Source/TejoWarsTP.Target.cs
+++ Source/TejoWarsTP.Target.cs
@@ -8,8 +8,8 @@ public class TejoWarsTPTarget : TargetRules
 	public TejoWarsTPTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("TejoWarsTP");
 	}
 }
